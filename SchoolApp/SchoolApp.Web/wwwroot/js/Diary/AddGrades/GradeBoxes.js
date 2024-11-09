80d51f4b74b47dda9943dd3e3ac231fc253694c6
Kristian Ivanov <k.antoanov@gmail.com>
2024-11-09 18:21:52 +0200

    Reorganized JS files

---
+++ SchoolApp/SchoolApp.Web/wwwroot/js/Diary/AddGrades/GradeBoxes.js
@@ -0,0 +1,29 @@
+﻿
+
+document.addEventListener('DOMContentLoaded', function () {
+    const rows = document.querySelectorAll('tbody tr');
+    rows.forEach(row => {
+        const gradeBoxes = row.querySelectorAll('.grade-box');
+        const gradeDisplay = row.querySelector('.grade-display');
+        const realGradeInput = row.querySelector('.real-grade-value');
+
+        // Set initial values
+        if (row.querySelector('.grade-box.selected').getAttribute('data-grade') === '0') {
+            gradeDisplay.value = '';
+            realGradeInput.value = '0';
+        }
+
+        gradeBoxes.forEach(box => {
+            box.addEventListener('click', function () {
+                // Remove selected class from all boxes in this row
+                gradeBoxes.forEach(b => b.classList.remove('selected'));
+                // Add selected class to clicked box
+                this.classList.add('selected');
+                // Update grade display and hidden value
+                const grade = this.getAttribute('data-grade');
+                gradeDisplay.value = grade === '0' ? '' : grade;
+                realGradeInput.value = grade; // Винаги запазваме реалната стойност
+            });
+        });
+    });
+});
---
+++ SchoolApp/SchoolApp.Web/wwwroot/js/Diary/DiaryContent.js
@@ -0,0 +1,165 @@
+﻿
+
+$(document).ready(function () {
+    let selectedClassId = null;
+    let selectedSubjectId = null;
+    let currentTab = 'grades';
+
+    $(".btn-check").click(function () {
+
+        selectedClassId = $(this).next("label").data("class-id");
+        localStorage.setItem("selectedClassId", selectedClassId);
+
+        switch (currentTab) {
+            case 'absences':
+                loadAbsencesContent();
+                break;
+            case 'grades':
+                loadContent();
+                break;
+            case 'remarks':
+                loadRemarkContent();
+                break;
+        }
+    });
+
+    $(document).on("click", ".list-group-item", function () {
+
+        selectedSubjectId = $(this).attr("id");
+        localStorage.setItem("selectedContentId", selectedSubjectId);
+
+        if (selectedClassId != null && selectedSubjectId != null) {
+            // Вместо директно да зареждаме оценките, проверяваме кой е текущият раздел
+            switch (currentTab) {
+                case 'grades':
+                    loadGradeContent();
+                    break;
+                case 'remarks':
+                    loadRemarkContent();
+                    break;
+                case 'absences':
+                    loadAbsencesContent();
+                    break;
+            }
+        }
+    });
+
+    $(document).on("click", ".nav-item", function (e) {
+        let target = $(e.target).attr('data-target'); // Взима data-target атрибута
+
+        currentTab = target;
+
+        switch (target) {
+            case 'absences':
+                if (selectedClassId != null) {
+                    $('#addAbsenceButton').show();
+                    $('#addGradeButton').hide();
+                    $('#addRemarkButton').hide();
+                    loadAbsencesContent();
+                }
+                break;
+            case 'grades':
+                if (selectedClassId != null) {
+                    $('#addGradeButton').show();
+                    $('#addRemarkButton').hide();
+                    $('#addAbsenceButton').hide();
+                    loadGradeContent();
+                }
+                break;
+            case 'remarks':
+                if (selectedClassId != null) {
+                    $('#addRemarkButton').show();
+                    $('#addGradeButton').hide();
+                    $('#addAbsenceButton').hide();
+                    loadRemarkContent();
+                }
+                break;
+        }
+    });
+
+    $(document).on("click", "#addGradeButton button", function (e) {
+        if (selectedClassId != null && selectedSubjectId != null) {
+            $("#hiddenClassId").val(selectedClassId);
+            $("#hiddenSubjectId").val(selectedSubjectId);
+        } else {
+            e.preventDefault();
+            alert("Моля, изберете клас и предмет преди да добавите оценка.");
+        }
+    });
+
+    function loadContent() {
+        $.ajax({
+            url: '/Diary/LoadClassAndContent',
+            type: 'GET',
+            data: { classId: selectedClassId },
+            success: function (response) {
+                $("#content").html(response);
+                if (currentTab !== 'grades') {
+                    $('#subjectsSidebar').hide();
+                    $('#mainContentContainer').removeClass('col-lg-10').addClass('col-lg-12');
+                } else {
+                    $('#subjectsSidebar').show();
+                    $('#mainContentContainer').removeClass('col-lg-12').addClass('col-lg-10');
+                }
+            },
+            error: function () {
+                alert("Възникна грешка при зареждането на съдържанието.");
+            }
+        });
+    }
+
+    function loadGradeContent() {
+        $.ajax({
+            url: '/Diary/LoadGradeContent',
+            type: 'GET',
+            data: { classId: selectedClassId, subjectId: selectedSubjectId },
+            success: function (response) {
+                $("#main-content").html(response);
+                if (currentTab === 'grades') {
+                    $('#subjectsSidebar').show();
+                    $('#addGradeButton').show();
+                    $('#mainContentContainer').removeClass('col-lg-12').addClass('col-lg-10');
+                }
+            },
+            error: function () {
+                alert("Възникна грешка при зареждането на съдържанието.");
+            }
+        });
+    }
+
+    function loadRemarkContent() {
+        $.ajax({
+            url: '/Diary/LoadRemarksContent',
+            type: 'GET',
+            data: { classId: selectedClassId },
+            success: function (response) {
+                $("#main-content").html(response);
+                if (currentTab === 'remarks') {
+                    $('#subjectsSidebar').hide();
+                    $('#mainContentContainer').removeClass('col-lg-10').addClass('col-lg-12');
+                }
+            },
+            error: function () {
+                alert("Възникна грешка при зареждането на съдържанието.");
+            }
+        });
+    }
+
+    function loadAbsencesContent() {
+        $.ajax({
+            url: '/Diary/LoadAbsencesContent',
+            type: 'GET',
+            data: { classId: selectedClassId, subjectId: selectedSubjectId },
+            success: function (response) {
+                $("#main-content").html(response);
+                if (currentTab === 'absences') {
+                    $('#subjectsSidebar').hide();
+                    $('#mainContentContainer').removeClass('col-lg-10').addClass('col-lg-12');
+                }
+            },
+            error: function () {
+                alert("Възникна грешка при зареждането на съдържанието.");
+            }
+        });
+    }
+});
