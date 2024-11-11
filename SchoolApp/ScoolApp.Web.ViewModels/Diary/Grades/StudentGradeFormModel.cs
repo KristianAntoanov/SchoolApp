464f945ddaa9c16b5912fe5da9ac63f20ea4b585
Kristian Ivanov <k.antoanov@gmail.com>
2024-11-11 20:47:40 +0200

    Clean up Diary view models no longer in use

--- SchoolApp/ScoolApp.Web.ViewModels/Diary/Grades/StudentGradeFormModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SchoolApp.Web.ViewModels
-{
-	public class StudentGradeFormModel
-	{
-		public int StudentId { get; set; }
-
-		public int Grade { get; set; }
-	}
-}
--- SchoolApp/ScoolApp.Web.ViewModels/Diary/New/SubjectModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SchoolApp.Web.ViewModels.Diary.New
-{
-	public class SubjectModel
-	{
-        public int Id { get; set; }
-
-        public string SubjectName { get; set; } = null!;
-    }
-}
