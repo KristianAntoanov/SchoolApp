a4365123caebf2314b4c4d3a28c2dbafcd7774ce
Kristian Ivanov <k.antoanov@gmail.com>
2024-11-15 18:06:42 +0200

    Edit AbsencesViewModel and add new view model

--- SchoolApp/ScoolApp.Web.ViewModels/Diary/Absences/AbsencesViewModel.cs
+++ SchoolApp/ScoolApp.Web.ViewModels/Diary/Absences/AbsencesViewModel.cs
@@ -2,6 +2,8 @@
 {
 	public class AbsencesViewModel
 	{
+        public int Id { get; set; }
+
         public string SubjectName { get; set; } = null!;
 
         public bool IsExcused { get; set; }
---
+++ SchoolApp/ScoolApp.Web.ViewModels/Diary/Remarks/EditRemarkViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolApp.Web.ViewModels.Diary.Remarks
+{
+	public class EditRemarkViewModel
+	{
+		public int Id { get; set; }
+
+		public DateTime AddedOn { get; set; }
+
+        public int SubjectId { get; set; }
+
+        public int StudentId { get; set; }
+
+        public string RemarkText { get; set; } = null!;
+    }
+}
