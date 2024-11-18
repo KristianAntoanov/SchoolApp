ee3c762f1753d75c8f1ff7be0fe2556f25b63682
Kristian Ivanov <k.antoanov@gmail.com>
2024-11-18 19:13:07 +0200

    Add error messages and application constants to common layer

--- SchoolApp/SchoolApp.Common/ApplicationConstants.cs
+++ SchoolApp/SchoolApp.Common/ApplicationConstants.cs
@@ -3,5 +3,14 @@
 	public class ApplicationConstants
 	{
         public const string DateFormat = "dd/MM/yyyy";
+
+        public const int GradeMinValue = 0;
+        public const int GradeMaxValue = 6;
+
+        public const int NameMinLength = 3;
+        public const int NameMaxLength = 50;
+
+        public const int RemarkTextMinLength = 5;
+        public const int RemarkTextMaxLength = 300;
     }
 }
---
+++ SchoolApp/SchoolApp.Common/ErrorMessages.cs
@@ -0,0 +1,13 @@
+﻿namespace SchoolApp.Common
+{
+	public class ErrorMessages
+	{
+        //RequiredMessages
+        public const string DateRequiredMessage = "Добавянето на дата е задължително.";
+        public const string RemarkRequiredMessage = "Моля добавете забележка.";
+
+        public const string DateAfterMessage = "Данните не могат да бъдат въведени за бъдещи дни";
+
+        public const string RemarkTextStringLengthMessage = "Забележката трябва да е между {2} и {1} символа";
+    }
+}
