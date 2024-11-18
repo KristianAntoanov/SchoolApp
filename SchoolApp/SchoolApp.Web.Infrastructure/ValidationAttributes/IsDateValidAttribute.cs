2b47103c99795ea37de437299f31013d098abc2f
Kristian Ivanov <k.antoanov@gmail.com>
2024-11-18 19:16:31 +0200

    Add custom date validation attribute

---
+++ SchoolApp/SchoolApp.Web.Infrastructure/ValidationAttributes/IsDateValidAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolApp.Web.Infrastructure.ValidationAttributes
+{
+    public class IsDateValidAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is DateTime dateValue)
+            {
+                if (dateValue.Date > DateTime.Today)
+                {
+                    return new ValidationResult(ErrorMessage);
+                }
+            }
+            else
+            {
+                return new ValidationResult("Невалиден формат!");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
