30c30bfd56032a9b052b247e187c751427af4a85
Kristian Ivanov <k.antoanov@gmail.com>
2024-11-22 21:01:04 +0200

    Add some controllers for Admin area

---
+++ SchoolApp/SchoolApp.Web/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Data;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace SchoolApp.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class AdminBaseController : Controller
+    {
+        
+    }
+}
+
---
+++ SchoolApp/SchoolApp.Web/Areas/Admin/Controllers/StudentsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace SchoolApp.Web.Areas.Admin.Controllers
+{
+    public class StudentsController : AdminBaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
