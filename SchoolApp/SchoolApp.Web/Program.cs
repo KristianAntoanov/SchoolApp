0384f549f5f3da4a8eee56047bcbda165186d079
Kristian Ivanov <k.antoanov@gmail.com>
2024-11-22 20:59:58 +0200

    Create Admin area and configure shared view files

--- SchoolApp/SchoolApp.Web/Program.cs
+++ SchoolApp/SchoolApp.Web/Program.cs
@@ -46,9 +46,15 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+            name: "areas",
+            pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+    endpoints.MapDefaultControllerRoute();
+    endpoints.MapRazorPages();
+});
 
 app.MapRazorPages();
 
