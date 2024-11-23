c76332a6714b2ea48b0a1e96c29e67c80f042013
Kristian Ivanov <k.antoanov@gmail.com>
2024-11-23 21:45:06 +0200

    Add new ErrorMessages

--- SchoolApp/SchoolApp.Common/ErrorMessages.cs
+++ SchoolApp/SchoolApp.Common/ErrorMessages.cs
@@ -5,9 +5,14 @@
         //RequiredMessages
         public const string DateRequiredMessage = "Добавянето на дата е задължително.";
         public const string RemarkRequiredMessage = "Моля добавете забележка.";
+        public const string StudentNameRequiredMessage = "Името е задължително";
+        public const string ClassRequiredMessage = "Класът е задължителен";
+
+
+        //StringLengthMessages
+        public const string RemarkTextStringLengthMessage = "Забележката трябва да е между {2} и {1} символа";
+        public const string StudentNameStringLengthMessage = "Името трябва да е между {2} и {1} символа";
 
         public const string DateAfterMessage = "Данните не могат да бъдат въведени за бъдещи дни";
-
-        public const string RemarkTextStringLengthMessage = "Забележката трябва да е между {2} и {1} символа";
     }
 }
