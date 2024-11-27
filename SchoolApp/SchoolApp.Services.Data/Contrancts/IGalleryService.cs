ff4a4c4e1643c0aaa8a518078b3264e3322b6a5c
Kristian Ivanov <k.antoanov@gmail.com>
2024-11-27 21:22:19 +0200

    add service and contoller for gallery

---
+++ SchoolApp/SchoolApp.Services.Data/Contrancts/IGalleryService.cs
@@ -0,0 +1,9 @@
+﻿using SchoolApp.Web.ViewModels.Gallery;
+
+namespace SchoolApp.Services.Data.Contrancts
+{
+	public interface IGalleryService
+    {
+        Task<IEnumerable<AlbumViewModel>> GetAllAlbumsWithImagesAsync();
+    }
+}
