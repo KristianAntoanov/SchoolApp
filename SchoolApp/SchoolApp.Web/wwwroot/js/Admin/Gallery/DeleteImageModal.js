c0653a89bbaf103f512f9f08afd83c679b1929ed
Kristian Ivanov <k.antoanov@gmail.com>
2024-11-28 20:40:56 +0200

    Refactor: Extract JavaScript from views Admin Gallery into separate files

---
+++ SchoolApp/SchoolApp.Web/wwwroot/js/Admin/Gallery/DeleteImageModal.js
@@ -0,0 +1,7 @@
+﻿
+
+document.getElementById('deleteImageModal').addEventListener('show.bs.modal', function (event) {
+    var button = event.relatedTarget;
+    var imageId = button.getAttribute('data-image-id');
+    document.getElementById('imageIdToDelete').value = imageId;
+});
