ba7363dd5efefc131c4796e7f9560a329023f70d
Kristian Ivanov <k.antoanov@gmail.com>
2024-11-30 17:06:48 +0200

    Extract JavaScript from views

---
+++ SchoolApp/SchoolApp.Web/wwwroot/js/News/DeleteNewsModal.js
@@ -0,0 +1,10 @@
+﻿
+
+document.getElementById('deleteModal').addEventListener('show.bs.modal', function (event) {
+    var button = event.relatedTarget;
+    var newsId = button.getAttribute('data-news-id');
+    var newsTitle = button.getAttribute('data-news-title');
+
+    document.getElementById('newsTitle').textContent = newsTitle;
+    document.getElementById('deleteForm').action = '/News/Delete/' + newsId;
+});
