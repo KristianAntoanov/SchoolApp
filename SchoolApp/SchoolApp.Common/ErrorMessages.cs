42d8e5471bfe48327cce2b34a8bae3743c408d66
Kristian Ivanov <k.antoanov@gmail.com>
2024-12-02 13:43:00 +0200

    Refactor ErrorMessages

--- SchoolApp/SchoolApp.Common/ErrorMessages.cs
+++ SchoolApp/SchoolApp.Common/ErrorMessages.cs
@@ -7,15 +7,19 @@
         public const string RemarkRequiredMessage = "Моля добавете забележка.";
         public const string StudentNameRequiredMessage = "Името е задължително";
         public const string ClassRequiredMessage = "Класът е задължителен";
+
         public const string TeacherNameRequiredMessage = "Името на учител е задължително";
         public const string TeacherJobTitleRequiredMessage = "Длъжността на учител е задължителнa";
         public const string TeacherImageRequiredMessage = "Моля изберете снимка";
         public const string TeacherSubjectRequiredMessage = "Моля изберете поне един предмет";
+
         public const string GalleryImageRequiredMessage = "Моля изберете снимка.";
         public const string GalleryTitleRequiredMessage = "Заглавието е задължително";
         public const string GalleryDescriptionRequiredMessage = "Описанието е задължително";
+
         public const string AnnouncementTitleRequiredMessage = "Заглавието е задължително";
         public const string AnnouncementContentRequiredMessage = "Съдържанието е задължително";
+
         public const string NewsTitleRequiredMessage = "Заглавието е задължително";
         public const string NewsContentRequiredMessage = "Съдържание е задължително";
 
