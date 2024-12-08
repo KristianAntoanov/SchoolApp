8590f38c621a8f0146b3a86b482bee9995e804fc
Kristian Ivanov <k.antoanov@gmail.com>
2024-12-08 18:14:29 +0200

    Refactor JS

---
+++ SchoolApp/SchoolApp.Web/wwwroot/js/News/FadeUp.js
@@ -0,0 +1,13 @@
+﻿
+
+document.addEventListener('DOMContentLoaded', function () {
+    function aosInit() {
+        AOS.init({
+            duration: 500,
+            easing: 'ease-in-out',
+            once: true,
+            mirror: false
+        });
+    }
+    aosInit()
+});
