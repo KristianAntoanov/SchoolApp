7593f734cd3e80a284697136c37167342c4c65b1
Kristian Ivanov <k.antoanov@gmail.com>
2024-12-10 14:40:48 +0200

    Added about section and upgrade footer

---
+++ SchoolApp/SchoolApp.Web/Controllers/AboutController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolApp.Web.Controllers;
+
+[AllowAnonymous]
+public class AboutController : BaseController
+{
+    public IActionResult History()
+    {
+        return View();
+    }
+
+    public IActionResult Mission()
+    {
+        return View();
+    }
+
+    public IActionResult Specialties()
+    {
+        return View();
+    }
+
+    public IActionResult Schedule()
+    {
+        return View();
+    }
+}
